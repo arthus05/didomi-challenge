5a84f729cbd8adb891fe188b0986f1878f3b4673
Arthus Vinicius Cruz de Moura <avinicius.ctr@cquestcapital.com>
2025-05-22 22:09:24 -0300

    wip: give consent form

--- src/pages/GiveConsent/GiveConsent.tsx
+++ src/pages/GiveConsent/GiveConsent.tsx
@@ -3,35 +3,43 @@ import { PageLayout } from '../../layout/Page/Page'
 import './styles.scss'
 
 export const GiveConsent = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.target as HTMLFormElement)
+    const parsedData = Object.fromEntries(formData.entries())
+  }
+
   return (
     <PageLayout>
       <section className='give-consent'>
-        <form action='' className='give-consent__form'>
+        <form className='give-consent__form' onSubmit={handleSubmit}>
           <fieldset className='give-consent__form-user-data'>
-            <input type='text' id='name' placeholder='Name' />
-            <input type='text' id='email' placeholder='Email address' />
+            <input type='text' id='name' name='name' placeholder='Name' />
+            <input type='email' id='email' name='email' placeholder='Email address' />
           </fieldset>
 
           <p>I agree to:</p>
 
           <fieldset className='give-consent__form-consents'>
             <div>
-              <input type='checkbox' id='consent1' />
+              <input type='checkbox' id='consent1' name='consent1' value='Receive newsletter' />
               <label htmlFor='consent1'>Receive newsletter</label>
             </div>
 
             <div>
-              <input type='checkbox' id='consent2' />
+              <input type='checkbox' id='consent2' name='consent2' value='Be shown targeted ads' />
               <label htmlFor='consent2'>Be shown targeted ads</label>
             </div>
 
             <div>
-              <input type='checkbox' id='consent3' />
+              <input type='checkbox' id='consent3' name='consent3' value='Contribute to anonymous visit statistics' />
               <label htmlFor='consent3'>Contribute to anonymous visit statistics</label>
             </div>
           </fieldset>
 
-          <button className='give-consent__form-submit'>Give consent</button>
+          <button type='submit' className='give-consent__form-submit'>
+            Give consent
+          </button>
         </form>
       </section>
     </PageLayout>
